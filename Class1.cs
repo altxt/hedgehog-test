700c58ae07cbdcf0b1f8aa875be29dc5bf0301ad
Тимонин Алексей Геннадьевич <Timonin.AG@gazprom-neft.ru>
2021-02-21 00:54:32 +0500

    Examples of how reports differ: Property vs Property<bool>

---
+++ Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using Hedgehog.Linq;
+using Range = Hedgehog.Linq.Range;
+
+namespace Hedgehog_test
+{
+    public class Class1
+    {
+        protected void Run(Action action) => action();
+
+        [Test]
+        public void HH_ExceptionInPropertyVoid()
+        {
+            var property =
+                from x in Property.ForAll(Gen.Int32(Range.Constant(0, 0)))
+                select Run(() =>
+                {
+                    _ = 1 / x != 2; // line 21
+                });
+            property.Check(); // line 23
+        }
+
+        [Test]
+        public void HH_ExceptionInPropertyBool()
+        {
+            var property =
+                from x in Property.ForAll(Gen.Int32(Range.Constant(0, 0)))
+                select 1 / x != 2; // line 41
+            property.Check();
+        }
+
+    }
+}
